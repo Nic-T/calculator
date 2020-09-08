ace7510542c70003c410d29c86f30fc09dfa2422
Nic-T <nicolastantau2003@gmail.com>
2020-09-08 18:05:10 +0300

    added functionality to all number and operation buttons ,the C button works , have to work on the back button and dot and to resolve some bugs

---
+++ resources/index.js
@@ -0,0 +1,154 @@
+let numButtons = document.querySelectorAll(".numB")
+let operators = document.querySelectorAll(".operator")
+let equal = document.getElementById("equal")
+let screen= document.getElementById("content")
+let clear =document.getElementById("clear")
+let dot=document.getElementById("dot")
+
+let numbers = new Array();
+let op= new Array();
+let lastElement
+let strNr;
+let operator =0;
+let number = 0;
+let nextOperator;
+let operatorNr=0;
+let count = 0;
+numbers.push(0)
+
+clear.addEventListener("click",()=>{
+
+    while(op.length!=0){
+
+        op.pop()
+
+    }
+    while(numbers.length !=0){
+        numbers.pop()
+    }
+    count=0;
+    number=0;
+    screen.innerHTML= number;
+    numbers.push(0)
+})
+
+numButtons.forEach(number =>{
+    number.addEventListener('click',()=>{
+        
+        numbers[count]=numbers[count]*10+parseInt(number.innerHTML);
+        screen.innerHTML=numbers[count]
+    })
+})
+
+dot.addEventListener('click',()=>{
+
+    strNr= numbers[count].toString()
+    strNr +="."
+    numbers[count]= parseInt(strNr)
+
+})
+
+operators.forEach(operator =>{
+
+    operator.addEventListener('click',()=>{
+        numbers.push(0)
+        op.push(operator.innerHTML)
+        count++;
+        
+    })
+
+})
+
+equal.addEventListener('click',()=>{
+    
+    while(op.length != 0){
+
+        for(i=0;i<=count;i++){
+
+            if(op[i]=="x"){
+
+                number = operate("x",numbers[i],numbers[i+1]);
+                console.log(number);
+                op.splice(i,1);
+                numbers.splice(i,2,number);
+            }
+
+            if(op[i]=="/"){
+
+                number=operate("/",numbers[i],numbers[i+1]);
+                console.log(number);
+                op.splice(i,1);
+                numbers.splice(i,2,number);
+            }
+
+        }
+
+        for(i=0;i<=count;i++){
+
+            if(op[i]=="-"){
+
+                number=operate("-",numbers[i],numbers[i+1]);
+                console.log(number);
+                op.splice(i,1);
+                numbers.splice(i,2,number);
+
+
+            }
+
+            if(op[i]=="+"){
+
+                number=operate("+",numbers[i],numbers[i+1]);
+                console.log(number);
+                op.splice(i,1);
+                numbers.splice(i,2,number);
+            }
+        }
+        console.log(numbers)
+    }
+    screen.innerHTML= number;
+    
+    numbers.pop()
+    numbers.push(0)
+    count=0;
+    
+    
+})
+
+
+
+function add(a,b) {
+    console.log("added")
+    return a+b;
+}
+
+function subtract(a,b){
+    console.log("subtracted")
+    return a-b;
+}
+
+function multiply(a,b){
+    console.log("multiplied")
+    return a*b;
+}
+
+function divide(a,b){
+    console.log("divided")
+    return a/b;
+}
+
+function operate(operator,a,b){
+
+    switch(operator){
+
+        case '+':
+            return add(a,b);
+        case '-':
+            return subtract(a,b);
+        case 'x':
+            return multiply(a,b);
+        case '/':
+            return divide(a,b);
+    }
+
+}
+
